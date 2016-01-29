413ec0b1cc918ba698eef325270d1c558baa9c08
Jojo <joana.schlizio@gmx.de>
2016-01-29 20:23:22 +0100

    3 2 1 Risiko
    Kühe wander
    Timer geht runter

---
+++ Assets/Scripts/CowCreator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CowCreator : MonoBehaviour {
+
+    public GameObject cow;
+    public int cowCount = 0;
+    public int cowMax = 20;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (cowCount < cowMax) {
+            cowCount++;
+            Instantiate(cow, new Vector3(250, 1, 250), Quaternion.identity);
+        }
+	
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    float timeLeft = 120.0f;
+    public Text text;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        text.text = "Time Left: " + Mathf.Round(timeLeft);
+        if (timeLeft < 0)
+        {
+            //Application.LoadLevel("gameOver");
+            text.text = "Game Over";
+        }
+    }
+}
