a0a475e95c76497365e62e9ec1ce6a9dc39dd982
CapsE <larskrafft@yahoo.de>
2016-01-29 19:08:18 +0100

    Testground for UfoMotor.cs

---
+++ Assets/Scripts/UfoMotor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UfoMotor : MonoBehaviour {
+
+    public float upForce = 1;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(Vector3.up * upForce);
+	}
+}
