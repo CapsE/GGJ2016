4deb43298c779ba49997aaa2656a6a4713113989
CapsE <larskrafft@yahoo.de>
2016-01-30 20:35:42 +0100

    New StartSzene Added Cow-Shaker

---
+++ Assets/Scripts/CowShaker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CowShaker : MonoBehaviour {
+
+    private float time = 0;
+    private float timeB = 0;
+    private Vector3 angles;
+
+	// Use this for initialization
+	void Start () {
+        angles = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        timeB += Time.deltaTime;
+        if (time > 0.125f) {
+            time = 0;
+            angles = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+        }
+        transform.Rotate(angles);
+        if(timeB > 1) {
+            timeB = 0;
+            transform.rotation = transform.parent.rotation;
+        }
+	}
+}
---
+++ Assets/Scripts/RandomMooh.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMooh : MonoBehaviour {
+
+    public float factor = 10;
+
+    private AudioSource audioSource;
+    private float chance = 0;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        chance += 0.1f;
+        float x = Random.Range(0.0f, chance);
+        if(chance >= factor && !audioSource.isPlaying) {
+            audioSource.pitch = Random.Range(0.6f, 1.5f);
+            audioSource.Play();
+            chance = 0;
+        }
+	}
+}
