f745b168f2f449c38eea44ec21aa84f814494821
Jojo <joana.schlizio@gmx.de>
2016-01-30 12:22:38 +0100

    jooo

--- Assets/Scripts/CowCreator.cs
+++ Assets/Scripts/CowCreator.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CowCreator : MonoBehaviour {
 
     public GameObject cow;
-    public static int cowCount = 0;
+    public static int cowCount = 20;
     public int cowMax = 20;
 
 	// Use this for initialization
