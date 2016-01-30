21ad100d2301d6f85fa7f512548f2dc61a1d5aea
Jojo <joana.schlizio@gmx.de>
2016-01-30 14:37:48 +0100

    UfoRotate

---
+++ Assets/Scripts/UfoRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UfoRotate : MonoBehaviour {
+
+
+    public Vector3 rotateSpeed = new Vector3(0, 0.0001f, 0);
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(rotateSpeed);
+	}
+}
